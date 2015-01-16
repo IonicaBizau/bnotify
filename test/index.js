60af0f335d7dcefd484d2d887ce0532f55811ad0
Ionică Bizău <bizauionica@yahoo.com>
2015-01-16 16:11:41 +0200

    Added test

---
+++ test/index.js
@@ -0,0 +1,13 @@
+// Library
+var BNotify = require("../lib");
+
+// Show the notification
+BNotify({
+    title: "Test Notification"
+  , description: "Adipisicing molestias soluta consectetur debitis doloribus. Doloremque amet temporibus suscipit quis ipsum vitae rerum ad iure nulla repellat iure molestias. Provident reiciendis veritatis doloribus maxime eum repellendus aut possimus ab!"
+  , duration: 4000
+  , icon: "/bell.png"
+}, function () {
+    process.exit(0);
+});
+
