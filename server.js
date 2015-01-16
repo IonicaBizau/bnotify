7f57db29d32b80916b70e56e4b2e5955e8554480
Ionică Bizău <bizauionica@yahoo.com>
2015-01-16 12:37:47 +0200

    Server side.

--- server.js
+++ server.js
@@ -1,14 +1,63 @@
 // Dependencies
-var Lien = require("lien");
+var Lien = require("lien")
+  , WebSocketServer = require("ws").Server
+  , Exec = require("child_process").exec
+  , Ul = require("ul")
+  ;
+
+// Opened Windows
+var _windows = {};
 
 // Init lien server
-var server = new Lien({
+var app = new Lien({
     host: "localhost"
   , port: 1111
   , root: __dirname + "/public"
 });
 
 // Add page
-server.page.add("/", function (lien) {
+app.page.add("/", function (lien) {
     lien.file("/index.html");
 });
+
+app.page.add("/api/open", "post", function (lien) {
+    console.log(lien.data);
+    var id = Math.random().toString(36);
+
+    _windows[id] = lien.data = Ul.merge({
+        title: ""
+      , description: ""
+      , duration: 4000
+      , icon: ""
+    }, lien.data);
+
+    lien.data.title = lien.data.title.replace(/\"/g, "\\\"");
+    lien.data.description = lien.data.description.replace(/\"/g, "\\\"");
+
+    var command =
+        "bat -d http://localhost:1111 --tt -u \"F\" \"F\" \"F\" \"F\""
+        .replace("F", lien.data.title)
+        .replace("F", lien.data.description)
+        .replace("F", lien.data.icon)
+        .replace("F", lien.data.duration)
+        ;
+
+    Exec(command, function (err, stdout) {
+        console.log(err || stdout);
+        delete _windows[id];
+    });
+
+    lien.end(lien.data);
+});
+
+
+// Websocket server
+var wss = new WebSocketServer({ server: app._server });
+wss.on("connection", function(ws) {
+    ws.send(JSON.stringify(), function() {});
+    ws.on('close', function() {
+        console.log('stopping client interval');
+        clearInterval(id);
+    });
+});
+
